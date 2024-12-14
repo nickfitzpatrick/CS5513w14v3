4c587d63fa2b83b836707b552b9b3ee942d34001
Nick Fitzpatrick <nickf5624@gmail.com>
2024-12-13 18:18:32 -0800

    Currently Working

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dd38499552c35993"; }
+/******/ 		__webpack_require__.h = function() { return "812ed343032223ca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/[id].js
+++ pages/[id].js
@@ -7,7 +7,8 @@ export async function getStaticProps( { params } ) {
   return {
     props: {
       itemData
-    }
+    },
+    revalidate: 60
   };
 }
 
@@ -23,12 +24,26 @@ export async function getStaticPaths() {
 
 
 export default function Entry({ itemData }) {
+  console.log("Item Data: ", itemData);
+
+  const obj = itemData.scf_fields;
+  console.log("DATA IN OBJ: ", obj);
+  
+  // Use replace to format the string into JSON format
+  let formatString = '{"' + obj.replace(/,/g, '","').replace(/:/g, '":"') + '"}';
+  
+  // Parse the formatted string into a JSON object
+  let parsedObj = JSON.parse(formatString);
+  console.log("OBJ PARSED: ", parsedObj);
+
+
   const mediaPath = itemData.gif_path || itemData.img_path;
   return (
     <Layout>
       <article className="card col-12">
         <div className="card-body col-12">
           <h5 className="card-title" style={{fontWeight: 'bold', fontSize: '1.5rem'}}>{itemData.post_title}</h5>
+          <h4>{parsedObj.alian_description || parsedObj.character_description || parsedObj.yokai_description}</h4>
           <img
             src={`https://dev-nickfitzpatrick-5513-w11.pantheonsite.io/wp-content/uploads/${mediaPath}`}
             alt="Media"
